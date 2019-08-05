99e1ae52b4535e47e8d900ae5a02a53349eb70f9
minekucukal <minekucukal@gmail.com>
2019-08-06 02:35:37 +0300

    omdb search

--- main.js
+++ main.js
@@ -1,24 +1,43 @@
-$(document).ready( function() {
-    $('form').submit(function(event) {
-        event.preventDefault();
-      
-        // Get The value from the form
-        var search = $('#search').val();
-        var movieURL = 'https://www.omdbapi.com/?apikey=63f944af&t='+search;
-      
-        function movies(data) {
-        
-          var movieHTML = '<ul>';
-      
-          console.log(data.Title);
+$(document).ready(function () {
+  var movieResults;
 
-        //$.find(".movie_title").append(data.Title);
-        $('<img src="' + data.Poster+ '"</img>').appendTo('.movie_img');
-        $('<p>' + data.Title+ '</p>').appendTo('.movie_title');
-        $('<p>' + data.Genre+ '</p>').appendTo('.movie_text');
-        
-      
-        }    
-        $.getJSON(movieURL, movies);
-      });
+  function movieList(results) {
+    var newMovie = $("body").find("#movies");
+    $.each(results, function (i) {
+      var x = 
+      '<div class="movie col"><div class=""><div id="movie-image"><img src="' + results[i].Poster + 
+      '" id="image" class="image"></div><div class="informations"><span id="title" class="title">'+ results[i].Title +'</span></div></div></div>';
+      newMovie.append(x);
     });
+  }
+
+  function removeStorage(){
+    localStorage.removeItem('movieResults');
+    $('#movies').empty();
+
+  }
+
+  $('#remove').click(function() {
+    removeStorage();
+  });
+
+  if (localStorage.getItem('movieResults') != null) {
+    movieResults = JSON.parse(localStorage.getItem('movieResults'));
+    movieList(movieResults);
+  }
+  $('form').submit(function (event) {
+    event.preventDefault();
+
+    // Get The value from the form
+    var search = $('#search').val();
+    var movieURL = 'https://www.omdbapi.com/?apikey=63f944af&s=' + search;
+
+    function movies(data) {
+        movieResults = data.Search;
+        movieList(movieResults);
+        localStorage.setItem('movieResults', JSON.stringify(movieResults));
+    }
+    $.getJSON(movieURL, movies);
+
+  });
+});
